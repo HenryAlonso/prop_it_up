47fece2b6c5e116b6e00d2f7c70dc8bb1904a2a1
Henry Alonso <henryalonso78@gmail.com>
2023-05-09 20:33:29 -0400

    Prop it up!

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import PersonCard from './components/PersonCard.js'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Prop it Up!!</h1>
+      <PersonCard firstName={"Henry"} lastName={"Alonso"} age={29} hairColor={"Brown"}/>
+      <PersonCard firstName={"Myleya"} lastName={"Morton"} age={28} hairColor={"Brown"}/>
+      <PersonCard firstName={"Rodney"} lastName={"Jackson"} age={21} hairColor={"Blue"}/>
+      <PersonCard firstName={"Bobby"} lastName={"Sullivan"} age={56} hairColor={"Blond"}/>
     </div>
   );
 }
